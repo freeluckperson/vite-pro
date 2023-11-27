3c906f829d9bd5f9aa959a6d5c6d7665a78d52ba
freeluckperson <gestionecesaria@gmail.com>
2023-11-27 10:15:15 -0400

    27/11/2023

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,42 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  // return (
-  //   <>
-  //     <div>
-  //       <a href="https://vitejs.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.jsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </>
-  // )
-  return <h3>Hola mundo</h3>
+const CardLayout = () => {
+  const cardData = [
+    { title: 'Card 1', description: 'Description for Card 1' },
+    { title: 'Card 2', description: 'Description for Card 2' },
+    { title: 'Card 3', description: 'Description for Card 3' },
+    { title: 'Card 4', description: 'Description for Card 4' },
+    { title: 'Card 5', description: 'Description for Card 5' },
+    { title: 'Card 6', description: 'Description for Card 6' },
+];
+
+  return (
+    <div className="card-layout">
+      {cardData.map((card, index) => (
+        <Card key={index} title={card.title} description={card.description} />
+      ))}
+    </div>
+  );
+};
+
+const Card = ({ title, description }) => {
+  return (
+    <div className="card">
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <h1>Card Layout</h1>
+      <CardLayout />
+    </div>
+  );
 }
 
 export default App
